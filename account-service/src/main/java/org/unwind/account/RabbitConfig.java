e724d963fa68d549e8accdce6080ef9d2b974d07
Mazennaji <mazen.naji.1@gmail.com>
2026-08-16 20:21:12 +0300

    Fix message conversion: use SimpleMessageConverter for raw-message listener

--- account-service/src/main/java/org/unwind/account/RabbitConfig.java
+++ account-service/src/main/java/org/unwind/account/RabbitConfig.java
@@ -1,11 +1,13 @@
 package org.unwind.account;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.unwind.common.Messaging;
@@ -53,9 +55,18 @@ public class RabbitConfig {
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory cf, MessageConverter converter) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory cf, MessageConverter jsonConverter) {
         RabbitTemplate template = new RabbitTemplate(cf);
-        template.setMessageConverter(converter);
+        template.setMessageConverter(jsonConverter);
         return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new SimpleMessageConverter());
+        return factory;
     }
 }
